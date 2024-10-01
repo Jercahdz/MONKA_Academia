4481a9fd96fd644a9484d6e960a01eda781afd2d
Juan Diego Marin Herrera <94065036+JuanM-25@users.noreply.github.com>
2024-10-01 16:29:25 -0600

    1/10/2024 Commit JuanDiego
    Archivo conexion php.
    Cambiar los primeros datos a los suyos.

---
+++ php/conexion.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Ryzen@Nitro5@";
+$database = "AcademiaMonka"; #Pendiente
+
+try {
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
