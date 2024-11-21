ed652b65b484d7f12f153db316087d56e7b8869b
Jercahdz <hejohc@gmail.com>
2024-11-21 16:39:42 -0600

    21/11/2024 Commit Jerson
    Se cambio el link de chatbot de script.js

--- js/script.js
+++ js/script.js
@@ -208,7 +208,7 @@ function handleSendMessage() {
 
 		// Enviar el mensaje al backend con AJAX
 		$.ajax({
-			url: 'php/chatbot.php',       // Tu archivo backend PHP
+			url: 'php/chatbot/chatbot.php',       // Tu archivo backend PHP
 			type: 'POST',
 			data: { text: message },
 			success: function (response) {
