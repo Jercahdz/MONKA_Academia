ad8ab69ae76cbc107b5484bea09b7b30286fd6b6
Jercahdz <hejohc@gmail.com>
2024-12-10 23:01:45 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,18 +1,13 @@
 <?php
-$db_host = getenv('DB_HOST') . ":3306";
+$db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASSWORD');
 $db_name = getenv('DB_NAME');
 
-try {
-    // Crear la conexion
-    $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-    // Verificar la conexion
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed: " . $conn->connect_error);
-    }
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
+if ($conn->connect_error) {
+    die("Error en la conexión: " . $conn->connect_error);
 }
+echo "Conexión exitosa a la base de datos";
 ?>
